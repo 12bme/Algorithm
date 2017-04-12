312cbbd493b165b8ae8770c44f9b2541342eb9dc
jiseonoh <jiseonoh@DESKTOP-Q4E0CC3>
2017-04-12 20:58:11 +0900

    20170412 오지선 1000문제 ( 두 수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오 )

---
+++ src/backjoon/P1000.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backjoon;
+import java.util.Scanner;
+
+/**
+ *
+ * @author jiseonoh
+ */
+public class P1000 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int firstInteger;
+        int secondInteger;
+        while(true){
+            firstInteger = sc.nextInt();
+            if(firstInteger > 0) break;
+        }
+        while(true){
+            secondInteger = sc.nextInt();
+            if(secondInteger < 10) break;
+        }
+        System.out.println(firstInteger+secondInteger);
+    }
+}
