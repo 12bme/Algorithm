289678db58478a1c5e9c22876ae8d8a801ae6bde
jiseonoh <jiseonoh@DESKTOP-Q4E0CC3>
2017-04-20 13:55:41 +0900

    20170420 오지선 문자열 뒤바꾸기

---
+++ src/algorithm/ReverseString.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm;
+
+/**
+ *
+ * @author jiseonoh
+ */
+class ReverseString {
+    public void reverseString(String input) {
+        for(int i=(input.length()-1); i>=0; i--) {
+            System.out.println(input.charAt(i));
+        }
+    }
+    public void reverseBuffer(String input) {
+        // 버퍼 생성
+        StringBuffer stringBuffer = new StringBuffer();
+        // 버퍼에 변환할 문자열을 넣고,
+        stringBuffer.append(input);
+        // 버퍼 안의 reverse()를 이용해 거꾸로 출력.
+        System.out.print(stringBuffer.reverse());
+    }
+}
