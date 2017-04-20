6232b999cb8d26635a040e1c37faf4b07cf8681f
jiseonoh <jiseonoh@DESKTOP-Q4E0CC3>
2017-04-20 13:55:27 +0900

    20170420 오지선 스택을 이용한 큐 구현

---
+++ src/algorithm/StackQueue.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithm;
+
+import java.util.*;
+
+/**
+ *
+ * @author jiseonoh
+ */
+public class StackQueue<E> {
+    /*
+        How to implement a queue using two stack?
+        1) keep 2 Stacks, let's call them inbox and outbox.
+            Enqueue : Push the new element onto inbox
+            Dequeue :
+                - If outbox is empty, refill it by popping each element from
+                    inbox and pushing it onto outbox.
+                - Pop and return the top element from outbox.
+        2) Using this method, each element will be in each stack exactly once -
+           meaning each element will be pushed twice and popped twice,
+           giving amortized constant time operations.
+    */
+    public class Queue<E> {
+        private Stack<E> inbox = new Stack<E>();
+        private Stack<E> outbox = new Stack<E>();
+        
+        public void enqueue(E item) {
+            inbox.push(item);
+        }
+        
+        public E dequeue() {
+            if(outbox.isEmpty()) {
+                while(!inbox.isEmpty()) {
+                    outbox.push(inbox.pop());
+                }
+            }
+            return outbox.pop();
+        }
+    }
+    
+    
+}
