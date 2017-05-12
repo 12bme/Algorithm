8e1d2e963d0dd39476f9a39bfe346c33948c5f7a
jiseonoh <jiseon121@gmail.com>
2017-05-12 20:43:05 +0900

    add ShellSort

--- src/algorithm/sort/ShellSort.java
+++ src/algorithm/sort/ShellSort.java
@@ -11,7 +11,34 @@ package algorithm.sort;
  */
 public class ShellSort {
     public static void main(String[] agrs) {
+        int[] arr = {69, 10, 30, 2, 16, 8, 31, 22};
+        shellSort(arr);
+    }
+    public static void intervalSort(int arr[], int begin, int end, int interval) {
+        int item = 0;
+        int j = 0;
         
+        for(int i = begin + interval; i <= end; i = i + interval) {
+            item = arr[i];
+            for (j = i - interval; j >= begin && item < arr[j]; j-= interval)
+                arr[j + interval] = arr[j];
+            arr[j + interval] = item;
+        }
+    }
+    public static void shellSort(int arr[]) {
+        int interval = 0;
+        int t = 1;
+        int arrSize = arr.length;
+        
+        interval = arrSize / 2;
+        
+        while(interval >= 1) {
+            for(int i = 0; i < interval; i++) {
+                intervalSort(arr, i, arrSize -1, interval);
+                System.out.println("셸 정렬" + t++ + "단계: interval => " + interval);
+                interval /= 2;
+            }
+        }
     }
 }
 
